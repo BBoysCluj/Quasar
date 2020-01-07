a745d134864b4bbae5c868f2a32c266179349e62
Adrian <interaliaconsultants@gmail.com>
2020-01-07 18:40:29 +0200

    initial commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore necessitatibus eum error nulla dolores fugiat expedita libero odio dicta, consequuntur repudiandae ipsam praesentium officiis sequi quia eaque ducimus quisquam atque.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, sint nulla saepe explicabo pariatur voluptas modi quaerat labore qui voluptates placeat beatae voluptatem provident, dicta, architecto alias ratione enim. Atque.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
